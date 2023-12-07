2fe50f448dab43a6d55253ab2ab6809def7a5cab
Isaac Soares <isaacsoares.trabalho@gmail.com>
2023-12-06 21:43:36 -0300

    relaciona js e html

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 document.entrada.addEventListener('submit', leFormulario);
 
+const olJoao = document.querySelector("#olJoao");
+const olMaria = document.querySelector("#olMaria");
+
 function leFormulario(event){
   event.preventDefault();
   const fruta = document.entrada.fruta.value;
